405ef3860030a71e834c6a80338be714e52943d7
crashlaker <>
2021-01-14 14:41:24 +0000

    separate navbar from app

--- src/App.js
+++ src/App.js
@@ -12,34 +12,8 @@ const app = (props) => {
                 component={elem.component}></Route>
   })
 
-  const onNavSelect = path => {
-    console.log('onnavselect', path)
-    props.history.push(`${path}`)
-  }
-
-  const navItems = routes.map(d => {
-    return (
-      <li key={d.title}>
-        <a href="#!" onClick={() => onNavSelect(d.path)}>{d.title}</a>
-      </li>
-    )
-  }).reverse()
-
   return (
     <div id="app">
-      <nav className="indigo">
-        <div className="nav-wrapper container">
-          <a href="#" className="brand-logo center">
-            Cloud<i className="material-icons">cloud</i>
-          </a>
-          <a href="#modal-login" className="right">
-            <i className="material-icons">login</i>
-          </a>
-          <ul id="nav-mobile" className="left hide-on-med-and-down">
-            {navItems}
-          </ul>
-        </div>
-      </nav>
       <div className="container" style={{'height': '30px'}}>
         <Switch>
           {routesView}
---
+++ src/Views/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import routes from './routes'
+
+const navbar = (props) => {
+
+  const onNavSelect = path => {
+    console.log('onnavselect', path)
+    props.history.push(`${path}`)
+  }
+
+  const navItems = routes.map(d => {
+    return (
+      <li key={d.title}>
+        <a href="#!" onClick={() => onNavSelect(d.path)}>{d.title}</a>
+      </li>
+    )
+  }).reverse()
+
+  return (
+    <nav className="indigo">
+      <div className="nav-wrapper container">
+        <a href="#" className="brand-logo center">
+          Cloud<i className="material-icons">cloud</i>
+        </a>
+        <a href="#modal-login" className="right">
+          <i className="material-icons">login</i>
+        </a>
+        <ul id="nav-mobile" className="left hide-on-med-and-down">
+          {navItems}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+
+
+export default navbar
